916402cfe33ebc6bf34ad0fa763e61bd70af61b4
it89 <ivan.tikhonov.spb@gmail.com>
2016-10-23 18:20:43 +0300

    Add StockPortfolio

---
+++ Journal/src/main/java/com/github/it89/investordiary/stockmarket/StockPortfolio.java
@@ -0,0 +1,85 @@
+package com.github.it89.investordiary.stockmarket;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+/**
+ * Клиентский портфель.
+ * Сейчас учитывается только прибыль, пополнений нет
+ */
+public class StockPortfolio {
+    /** Прибыль, с которой еще не удержан налог*/
+    private BigDecimal sumProfitNotTaxed = new BigDecimal(0);
+    /** Прибыль с учетом уже удержанного налога (включая прибыль, которая не облагается налогом*/
+    private BigDecimal sumProfitTaxed = new BigDecimal(0);
+    /** Сумма налога*/
+    private BigDecimal sumTax = new BigDecimal(0);
+    /** Спиок акций и их количество*/
+    private HashMap<Asset, Long> assetCountMap = new HashMap();
+
+    public BigDecimal getSumProfitNotTaxed() {
+        return sumProfitNotTaxed;
+    }
+
+    public void setSumProfitNotTaxed(BigDecimal sumProfitNotTaxed) {
+        this.sumProfitNotTaxed = sumProfitNotTaxed;
+    }
+
+    public HashMap<Asset, Long> getAssetCountMap() {
+        return assetCountMap;
+    }
+
+    public void setAssetCountMap(HashMap<Asset, Long> assetCountMap) {
+        this.assetCountMap = assetCountMap;
+    }
+
+    public BigDecimal getSumProfitTaxed() {
+        return sumProfitTaxed;
+    }
+
+    public void setSumProfitTaxed(BigDecimal sumProfitTaxed) {
+        this.sumProfitTaxed = sumProfitTaxed;
+    }
+
+    public BigDecimal getSumTax() {
+        return sumTax;
+    }
+
+    public void setSumTax(BigDecimal sumTax) {
+        this.sumTax = sumTax;
+    }
+
+    /**
+     * Изменяет состояние портфеля.
+     * @param trade - сделка
+     */
+    public void add(Trade trade) {
+        BigDecimal volume = trade.getVolume();
+        Asset asset = trade.getAsset();
+        long amount = trade.getAmount();
+
+        if(trade.getOperation() == TradeOperation.BUY)
+            volume = volume.negate();
+        else
+            amount *= -1;
+
+        sumProfitNotTaxed = sumProfitNotTaxed.add(volume);
+        sumProfitNotTaxed = sumProfitNotTaxed.add(trade.getCommission().negate());
+        if(trade instanceof TradeBond) {
+            sumProfitNotTaxed = sumProfitNotTaxed.add(((TradeBond) trade).getAccumulatedCouponYield().negate());
+        }
+        if(assetCountMap.containsKey(asset)) {
+            assetCountMap.put(asset, assetCountMap.get(asset) + amount);
+        } else {
+            assetCountMap.put(asset, amount);
+        }
+    }
+
+    public void Add(AssetIncome assetIncome) {
+        BigDecimal volume = assetIncome.getVolume();
+        BigDecimal tax = assetIncome.getTax();
+
+        sumProfitTaxed = sumProfitTaxed.add(volume.add(tax.negate()));
+        sumTax = sumTax.add(tax);
+    }
+}
---
+++ Journal/src/test/java/com/github/it89/investordiary/stockmarket/StockPortfolioTest.java
@@ -0,0 +1,60 @@
+package com.github.it89.investordiary.stockmarket;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Axel on 23.10.2016.
+ */
+public class StockPortfolioTest {
+    @org.junit.Test
+    public void testAdd() throws Exception {
+        // Add Trade
+        Asset assetA = new Asset("A");
+        TradeStock tradeStock = new TradeStock("A1");
+        tradeStock.setAsset(assetA);
+        tradeStock.setOperation(TradeOperation.BUY);
+        tradeStock.setVolume(new BigDecimal("3480"));
+        tradeStock.setAmount(100);
+        tradeStock.setCommission(new BigDecimal("1.98"));
+
+        StockPortfolio sp = new StockPortfolio();
+        sp.add(tradeStock);
+        sp.add(tradeStock);
+        tradeStock.setOperation(TradeOperation.SELL);
+        tradeStock.setVolume(new BigDecimal("3005"));
+        tradeStock.setCommission(new BigDecimal("1.71"));
+        sp.add(tradeStock);
+        tradeStock.setOperation(TradeOperation.BUY);
+        tradeStock.setVolume(new BigDecimal("3069"));
+        tradeStock.setCommission(new BigDecimal("1.75"));
+        sp.add(tradeStock);
+        assertEquals("100 + 100 - 100 + 100 must be 200", new Long(200), sp.getAssetCountMap().get(assetA));
+        assertEquals("-3480 - 1.98 - 3480 - 1.98 + 3005 - 1.71 - 3069 - 1.75 must be -7031.42", new BigDecimal("-7031.42"), sp.getSumProfitNotTaxed());
+
+        Asset assetB = new Asset("B");
+        TradeBond tradeBond = new TradeBond("B1");
+        tradeBond.setAsset(assetA);
+        tradeBond.setOperation(TradeOperation.BUY);
+        tradeBond.setVolume(new BigDecimal("3000"));
+        tradeBond.setAmount(3);
+        tradeBond.setCommission(new BigDecimal("3"));
+        tradeBond.setAccumulatedCouponYield(new BigDecimal("50"));
+        sp.add(tradeBond);
+        assertEquals("-7031.42 - 3000 - 3 - 50 must be -10084.42", new BigDecimal("-10084.42"), sp.getSumProfitNotTaxed());
+
+        // Add AssetIncome
+        BigDecimal sumProfitNotTaxed = sp.getSumProfitNotTaxed();
+        AssetIncome assetIncome = new AssetIncome();
+        assetIncome.setVolume(new BigDecimal("197.25"));
+        assetIncome.setTax(new BigDecimal("26"));
+        sp.Add(assetIncome);
+        assetIncome.setVolume(new BigDecimal("7612"));
+        assetIncome.setTax(new BigDecimal("989"));
+        sp.Add(assetIncome);
+        assertEquals("sumProfitNotTaxed not changed", sumProfitNotTaxed, sp.getSumProfitNotTaxed());
+        assertEquals("197.25 - 26 + 7612 - 989 = 6794.25", new BigDecimal("6794.25"), sp.getSumProfitTaxed());
+        assertEquals("26 + 989 = 1015", new BigDecimal("1015"), sp.getSumTax());
+    }
+}
